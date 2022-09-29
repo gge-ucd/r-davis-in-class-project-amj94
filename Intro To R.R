221386bb17262da5104f725494785d3a05692b2e
Alex Johanson <ajohanson@ucdavis.edu>
2022-09-29 14:35:08 -0700

    Added both intro notes as well as class work.

---
+++ Intro To R.R
@@ -0,0 +1,19 @@
+1+1
+
+log(sqrt(2))
+exp(2)
+2^2
+exp(2)
+1==1
+2>=3
+x<-2
+x
+x^2
+x<-c(1,3,5,6)
+x
+x
+log(x)
+my_favorite_ice_cream<-"chocolate"
+dir.create(path = "Scripts")
+getwd()
+""
---
+++ Scripts/R Davis Class Notes 9-29-22.R
@@ -0,0 +1,56 @@
+1+1
+# make sure you hit command+return
+
+log(3)
+log(x, base = exp(3))
+a<-1
+a
+b<-2
+c<- -a+b
+x
+rm(c)
+c
+c<- -a+b
+c
+b<- 4
+a <- b
+b
+x<- c(3,5,7,20)
+log(x)
+?c
+
+# naming conventions for objects
+
+my_name <- "Alex"
+my_name
+
+
+log(x = 3, base = 2)
+log(3, base = 10)
+?base r dictionary?
+?dictionary
+
+?dictionary
+?
+?help
+"Alex" <- my_name
+
+my_name
+
+
+rm(a, Alex, b)
+
+rm(c)
+rm(x)
+rm(my_favorite_ice_cream)
+
+getmd
+getwd
+
+getwd()
+
+list.files(path = "c:\Users\Alexander Johanson\OneDrive\Desktop\R-DAVIS-2022\r-davis-in-class-projects-amj94-data")
+list.files(path = "C:/Users/Alexander Johanson/OneDrive/Desktop/R-DAVIS-2022/r-davis-in-class-project-amj94/data")
+list.files(data)
+list.files(path = "data")
+dir.create(path = "Scripts")
